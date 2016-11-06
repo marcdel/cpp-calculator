c598f5c7042c56b75106c65b33d40b4a52782835
Marc Delagrammatikas <delagrammatikas@gmail.com>
2016-11-06 09:17:56 -0800

    Inital commit.

--- BryanTest/main.cpp
+++ BryanTest/main.cpp
@@ -1,15 +1,65 @@
-//
-//  main.cpp
-//  BryanTest
-//
-//  Created by Marc Delagrammatikas on 11/6/16.
-//  Copyright © 2016 Marc Delagrammatikas. All rights reserved.
-//
-
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+int main()
+{
+    // Declare variables
+    int firstNumber = 0;
+    int secondNumber = 0;
+    int answer = 0;
+    char operation;
+    
+    
+    // Ask user to enter to two integers
+    cout << "Please enter first integer: " << endl;
+    cin >> firstNumber;
+    cout << "Please enter second integer: " << endl;
+    cin >> secondNumber;
+    
+    // Determine the operatoin to be used
+    
+    cout << "Please enter an operation to use:\n"
+    << "A\tAddition\n"
+    << "S\tSubtraction\n"
+    << "M\tMultiplication\n"
+    << "D\tDivision\n"
+    << "Please enter the operator now: " << endl;
+    cin >> operation;
+    operation = toupper(operation);
+    
+    // Determine the answer
+    
+    if (operation == 'A')
+    {
+        answer = firstNumber + secondNumber;
+    }
+    else if (operation == 'S')
+    {
+        if (firstNumber > secondNumber)
+            answer = firstNumber - secondNumber;
+        else(secondNumber > firstNumber);
+            answer = secondNumber - firstNumber;
+    }
+    else if (operation == 'M')
+    {
+        answer = firstNumber * secondNumber;
+    }
+    else if (operation == 'D')
+    {
+        if (firstNumber > secondNumber)
+            answer = firstNumber / secondNumber;
+        else (secondNumber > firstNumber);
+            answer = secondNumber / firstNumber;
+    }
+    else(operation != 'A' && operation != 'S' && operation != 'M' && operation != 'D');
+    {
+        cout << "Invalid input for operator.  Please use 'A, S, M, or D.'" << endl;
+    }
+    
+    cout << "The answer to your problem is: " << answer << endl;
+    
+    
     return 0;
 }
